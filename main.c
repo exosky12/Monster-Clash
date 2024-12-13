5160b59479d7bbdf8c16fee8fb74f80c4cf07cf7
exosky12 <exoskyv2@gmail.com>
2024-12-13 11:41:09 +0100

    feat: existeGame & createNewGame displays

--- main.c
+++ main.c
@@ -1,24 +1,148 @@
 #include "main.h"
 
+File file1Vide(void)
+{
+    File f;
+    f.t = NULL;
+    f.q = NULL;
+    return f;
+}
+
+File emphiler(File f, int x)
+{
+    Maillon *m;
+    m = (Maillon *)malloc(sizeof(Maillon));
+    if (m == NULL)
+    {
+        printf("Opération interdite");
+        exit(1);
+    }
+    m->v = x;
+    m->suiv = NULL;
+    if (estVide(f))
+    {
+        f.t = m;
+        f.q = m;
+    }
+    f.q->suiv = m;
+    f.q = m;
+    return f;
+}
+
+File supprimer(File f)
+{
+    Maillon *m;
+    if (f.t == NULL && f.q == NULL)
+    {
+        printf("Opération interdite");
+        exit(1);
+    }
+    if (f.t == f.q)
+    {
+        free(f.t);
+        return file1Vide();
+    }
+    m = f.t;
+    f.t = f.t->suiv;
+    free(m);
+    return f;
+}
+
+Boolean estVide(File f)
+{
+    return f.t == NULL && f.q == NULL;
+}
+
+int longueur(File f)
+{
+    int l = 0;
+    while (f.t != NULL)
+    {
+        l++;
+        f.t = f.t->suiv;
+    }
+    return l;
+}
+
+void afficher(File f)
+{
+    while (f.t != NULL)
+    {
+        printf("%d", f.t->v);
+        f.t = f.t->suiv;
+    }
+}
+
+int tete(File f)
+{
+    if (estVide(f))
+    {
+        printf("Opération interdite");
+        exit(1);
+    }
+    return f.t->v;
+}
+
+void existingGameDisplay(void)
+{
+  FILE *gameFile;
+  char gameName[100], playerName[100];
+  clearScreen();
+  printf("▁ ▂ ▄ ▅ ▆ ▇ █ Jouer une partie prédéfinie █ ▇ ▆ ▅ ▄ ▂ ▁\n\n");
+  printf("Entrer le nom du fichier correspondant à la partie > ");
+  scanf("%s", gameName);
+  gameFile = fopen(gameName, "rb");
+  if (gameFile == NULL){
+    printf("[ERREUR] Fichier introuvable\n");
+    return;
+  }
+  printf("Enter le pseudo du joueur > ");
+  scanf("%s", playerName);
+  printf("\n[CONTEXTE] Vous arrivez dans un corridor, bordé par deux falaises des monstres arrivent les uns après les autres.\n");
+  existingGame();
+}
+
+// Ajouter type de retour et paramètres
+void existingGame(void)
+{
+    // Ajouter code
+}
+
+void createNewGameDisplay()
+{
+  char playerName[100];
+  clearScreen();
+  printf("▁ ▂ ▄ ▅ ▆ ▇ █ Créer une nouvelle partie █ ▇ ▆ ▅ ▄ ▂ ▁\n\n");
+  printf("Entrer votre pseudo > ");
+  scanf("%s", playerName);
+  createNewGame();
+}
+
+// Ajouter type de retour et paramètres
+void createNewGame(void)
+{
+    // Ajouter code
+}
+
 void global(void){
-  bool quit = False;
-  int choix;
+  Boolean quit = False;
+  int choice;
   while(!quit){
-    printf("▁ ▂ ▄ ▅ ▆ ▇ █  MENU  █ ▇ ▆ ▅ ▄ ▂ ▁\n");
-    printf("\t1. jouer une partie prédéfinie\n");
-    printf("\t2. créer une nouvelle partie\n");
-    printf("\t3. afficher la liste des joueurs triée par nom\n");
-    printf("\t4. afficher la liste des joueurs triée par meilleur score\n");
-    printf("\t9. quitter\n");
-    printf("------------------------\n");
-    printf("Votre choix : ");
-    scanf("%d", &choix);
-    switch(choix){
+    clearScreen();
+    printf("\n\t▁ ▂ ▄ ▅ ▆ ▇ █  MENU  █ ▇ ▆ ▅ ▄ ▂ ▁\n\n\n");
+    printf("[1] Jouer une partie prédéfinie\n");
+    printf("[2] Créer une nouvelle partie\n");
+    printf("[3] Afficher la liste des joueurs triée par nom\n");
+    printf("[4] Afficher la liste des joueurs triée par meilleur score\n");
+    printf("[9] Quitter\n\n");
+    printf("Votre choix > ");
+    scanf("%d", &choice);
+    switch(choice){
       case 1:
-        printf("Jouer une partie prédéfinie\n");
+        existingGameDisplay();
         break;
       case 2:
-        printf("Créer une nouvelle partie\n");
+        createNewGameDisplay();
         break;
       case 3:
         printf("Afficher la liste des joueurs triée par nom\n");
@@ -27,6 +151,7 @@ void global(void){
         printf("Afficher la liste des joueurs triée par meilleur score\n");
         break;
       case 9:
+        printf("Au revoir...\n");
         quit = True;
         break;
       default:
@@ -34,4 +159,11 @@ void global(void){
         break;
     }
   }
+}
+
+void clearScreen(void)
+{
+    for (int i = 0; i < 50; i++) {
+        printf("\n");
+    }
 }
--- main.h
+++ main.h
@@ -2,11 +2,46 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef enum {False, True} bool;
-typedef struct maillon {
-  int v;
-  struct maillon *suiv;
-  } maillon, *Liste;
+typedef enum {False, True} Boolean;
 
+typedef struct maillon
+{
+    int v;
+    struct maillon *suiv;
+} Maillon;
+
+typedef struct
+{
+    Maillon *t;
+    Maillon *q;
+} File;
+
+typedef struct {
+    char nom[50];           // Nom du monstre
+    int niveau;             // Niveau du monstre (1, 2 ou 3)
+    int pointsVie;          // Points de vie du monstre
+    int pointsDegats;       // Points de dégâts infligés par attaque
+    char armes[5];          // Liste des armes disponibles ('P', 'F', 'C', 'O', '#')
+    int nbArmes;            // Nombre d'armes disponibles
+} Monstre;
+
+typedef struct {
+    Monstre *monstres;      // Tableau dynamique de monstres
+    int nbMonstres;         // Nombre de monstres dans le groupe
+    int type;               // Type du groupe : 1 pour séquentiel, 2 pour simultané
+} GroupeMonstres;
+
+File file1Vide(void);
+File emphiler(File f, int x);
+File supprimer(File f);
+Boolean estVide(File f);
+int longueur(File f);
+void afficher(File f);
+int tete(File f);
 
 void global(void);
+void clearScreen(void);
+void existingGameDisplay(void);
+void existingGame(void);
+void createNewGameDisplay();
+void createNewGame(void);
